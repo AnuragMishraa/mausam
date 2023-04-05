b1fc93916301293df01244fe5225a6e1b1ca5500
Anurag Mishra <cen.20bced17@silicon.ac.in>
2023-04-05 16:20:55 +0530

    sixth-commit

---
+++ frontend/src/components/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import "../style/card.css";
+
+const card = () => {
+  return (
+    <>
+    
+     
+    </>
+  );
+};
+
+export default card;
